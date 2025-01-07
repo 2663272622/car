ce029d35d62ebe80d8d01ec53bc277855392c792
Rock <2663272622@qq.com>
2025-01-07 21:59:17 +0800

    feat: 优化油耗计算

--- src/pages/common/oilwear/index.vue
+++ src/pages/common/oilwear/index.vue
@@ -14,7 +14,7 @@
               labelWidth='auto'
           >
             <up-form-item
-                label="行驶距离（公里）:"
+                label="行驶距离(km):"
                 borderBottom
                 ref="item1"
             >
@@ -24,12 +24,22 @@
               ></up-input>
             </up-form-item>
             <up-form-item
-                label="油量（升）:"
+                label="加油量(L):"
                 borderBottom
                 ref="item1"
             >
               <up-input
                   v-model="fuel"
+                  border="none"
+              ></up-input>
+            </up-form-item>
+            <up-form-item
+                label="今日油价:"
+                borderBottom
+                ref="item1"
+            >
+              <up-input
+                  v-model="youjia"
                   border="none"
               ></up-input>
             </up-form-item>
@@ -41,6 +51,8 @@
           <!-- 计算结果 -->
           <view v-if="fuelConsumption !== null" class="result">
             <text>油耗：{{ fuelConsumption }} L/100km</text>
+            <view v-if='youjiakm && youjiakm > 0'>每公里：{{youjiakm}} 元</view>
+
           </view>
   </view>
 </template>
@@ -51,6 +63,8 @@ import { ref } from 'vue';
     const distance = ref('');   // 行驶距离
     const fuel = ref('');       // 油量
     const fuelConsumption = ref(null); // 油耗
+    const youjia = ref("")
+    const youjiakm = ref("")
 
     // 计算油耗的函数
     const calculateFuelConsumption = () => {
@@ -65,7 +79,11 @@ import { ref } from 'vue';
 
       // 计算油耗
       fuelConsumption.value = ((parseFloat(fuel.value) / parseFloat(distance.value)) * 100).toFixed(2);
+      youjiakm.value = (parseFloat(fuelConsumption.value) * parseFloat(youjia.value) / 100).toFixed(3);
+      youjiakm.value =  youjiakm.value === "NaN" ? '' : youjiakm.value
     };
+
+
 </script>
 
 <style scoped>
